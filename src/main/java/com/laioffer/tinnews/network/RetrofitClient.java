d45c743da2a07e74d71be80474dfd8de3fc5d8c4
Zilin Lu <89592975+tarringlu@users.noreply.github.com>
2022-03-22 17:07:06 -0400

    Add files via upload

---
+++ src/main/java/com/laioffer/tinnews/network/RetrofitClient.java
@@ -0,0 +1,44 @@
+package com.laioffer.tinnews.network;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.laioffer.tinnews.MainActivity;
+import com.laioffer.tinnews.repository.NewsRepository;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static final String API_KEY = "340a0064c7fe47ebbcbb76be2a8d580d";
+    private static final String BASE_URL = "https://newsapi.org/v2/";
+
+    public static Retrofit newInstance(NewsRepository mainActivity) {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new HeaderInterceptor())
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+
+    private static class HeaderInterceptor implements Interceptor {
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request original = chain.request();
+            Request request = original
+                    .newBuilder()
+                    .header("X-Api-Key", API_KEY)
+                    .build();
+            return chain.proceed(request);
+        }
+    }
+
+}
